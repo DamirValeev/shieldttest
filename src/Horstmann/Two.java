f481be36e2ce1144a8cca99045c1ac5a0e441a8d
DamirValeev <dmr.val@mail.ru>
2018-03-09 13:59:16 +0400

    23

---
+++ src/Horstmann/Two.java
@@ -0,0 +1,10 @@
+package Horstmann;
+import static java.lang.System.out;
+import java.util.Date;
+
+public class Two {
+    public static void main(String[] args) {
+        Date dt = new Date();
+        out.print(dt.toString());
+    }
+}
