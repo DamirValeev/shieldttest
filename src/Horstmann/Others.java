2aca4346e64db17758dde528097cfe83a5a52642
DamirValeev <dmr.val@mail.ru>
2018-03-11 02:40:53 +0400

    24

--- src/Horstmann/Others.java
+++ src/Horstmann/Others.java
@@ -1,35 +1,52 @@
 package Horstmann;
 
+import java.time.*;
+
 import static java.lang.System.out;
 
 class A {
-    String name;
-    int quantity;
-    A(){
+    private String name;
+    private int quantity;
+
+    A() {
         name = "nothing";
         quantity = 0;
     }
+
     A(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 }
 
 class B extends A {
-    double price;
-    B(){
+    private double price;
+
+    B() {
         super();
     }
+
     B(String name, double price) {
         super(name);
         this.price = price;
+    }
 
+    void getInfo() {
+        out.println(getName() + " " + price);
     }
 }
 
 public class Others {
     public static void main(String[] args) {
-        B b = new B("privet", 777.0);
-        out.println(b.name);
-        out.println(b.price);
+        LocalDate ld = LocalDate.of(2018, 4, 5);
+        B b = new B("ds", 234);
+        b.getInfo();
     }
 }
--- src/Horstmann/Two.java
+++ src/Horstmann/Two.java
@@ -1,10 +1,81 @@
 package Horstmann;
+
 import static java.lang.System.out;
-import java.util.Date;
+
+/**
+ * @author Damir Valeev R.
+ */
+
+class Employee {
+    private double salary;
+    private String name;
+    static int cool = 0;
+
+    {
+        cool++;
+    }
+
+    static {
+        out.println("Класс использовался впервые ");
+    }
+
+    Employee() {
+        salary = 200;
+    }
+
+    Employee(String str) {
+        this();
+        this.name = str;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = this.salary * byPercent / 100;
+        this.salary += raise;
+    }
+
+    public static void tripSal(Employee x) {
+        x.raiseSalary(200);
+    }
+
+    public double getSalary() {
+        return (double)(salary);
+    }
+
+    public void getInfo(Employee obj) {
+        out.println(obj.salary + " " + obj.name);
+    }
+
+    public static void swap(Employee x, Employee y) { //метод содержит ссылки и не
+        Employee temp = x;                              //не поменяет местами ссылки на объекты
+        x = y;
+        y = temp;
+    }
+}
+
+class Manager extends Employee {
+    private double bonus;
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double getSalary() {
+        bonus += super.getSalary();
+        return bonus;
+    }
+}
 
 public class Two {
     public static void main(String[] args) {
-        Date dt = new Date();
-        out.print(dt.toString());
+        Employee harry = new Employee("ds");
+        Employee baddy = new Employee("строка");
+        baddy.getInfo(harry);
+        out.println(Employee.cool);
+        out.println();
+        Manager mark = new Manager();
+        out.println(mark.getSalary());
+        mark.setBonus(2000);
+        out.println(mark.getSalary());
     }
 }
